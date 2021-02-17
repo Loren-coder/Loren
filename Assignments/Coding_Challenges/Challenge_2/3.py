f823ca2187fc71e916604638d8e4afe6cc80b19c
Loren-coder <78279168+Loren-coder@users.noreply.github.com>
2021-02-17 17:47:14 -0500

    Update 3.py

--- Assignments/Coding_Challenges/Challenge_2/3.py
+++ Assignments/Coding_Challenges/Challenge_2/3.py
@@ -2,9 +2,10 @@
 
 string = 'hi dee hi how are you mr dee'
 
-print(string.count("hi"))
-print(string.count("dee"))
-print(string.count("how"))
-print(string.count("are"))
-print(string.count("you"))
-print(string.count("mr"))
+
+print ("hi" ,string.count("hi"))
+print("dee" ,string.count("dee"))
+print("how",string.count("how"))
+print("are",string.count("are"))
+print("you",string.count("you"))
+print("mr",string.count("mr"))

878b9a20e260654c8d1f86465d5e802926ac4faf
Loren-coder <78279168+Loren-coder@users.noreply.github.com>
2021-02-17 17:20:12 -0500

    Create 3.py

---
+++ Assignments/Coding_Challenges/Challenge_2/3.py
@@ -0,0 +1,10 @@
+# 3. Given a singe phrase, count the occurrence of each word
+
+string = 'hi dee hi how are you mr dee'
+
+print(string.count("hi"))
+print(string.count("dee"))
+print(string.count("how"))
+print(string.count("are"))
+print(string.count("you"))
+print(string.count("mr"))

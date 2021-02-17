bcdf2ce1651245ee7bbef2c07dd4fbeda2dc179f
Loren-coder <78279168+Loren-coder@users.noreply.github.com>
2021-02-17 16:56:15 -0500

    Create 2.py

---
+++ Assignments/Coding_Challenges/Challenge_2/2.py
@@ -0,0 +1,9 @@
+# 2. List overlap
+
+list_a = ['dog', 'cat', 'rabbit', 'hamster', 'gerbil']
+list_b = ['dog', 'hamster', 'snake']
+
+overlap=set(list_a) & set(list_b)
+unique=set(list_b) ^ set(list_a)
+print (overlap)
+print (unique)

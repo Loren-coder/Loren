343f41a479df57cd95ac5bb334edbeebd19c2394
Loren-coder <78279168+Loren-coder@users.noreply.github.com>
2021-02-18 16:24:52 -0500

    Update 4.py

--- Assignments/Coding_Challenges/Challenge_2/4.py
+++ Assignments/Coding_Challenges/Challenge_2/4.py
@@ -1,5 +1,7 @@
 # 4. User input
 age = input("What is your age? ")
+
 years=65-age
 
-print ("There is" + years + "years until you reach retirement")
+print years
+print "There is" + " "+str(years) + " years until you reach retirement"

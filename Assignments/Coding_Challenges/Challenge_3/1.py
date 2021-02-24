52970be7412663abde7d5419d64487fe02153b33
Loren-coder <78279168+Loren-coder@users.noreply.github.com>
2021-02-24 16:02:17 -0500

    Update 1.py

--- Assignments/Coding_Challenges/Challenge_3/1.py
+++ Assignments/Coding_Challenges/Challenge_3/1.py
@@ -1,1 +1,40 @@
+# 1. Simple directory tree
 
+
+import os
+
+os.mkdir(r"C:\draft_code")
+
+os.mkdir(r"C:\draft_code\pending")
+
+os.mkdir(r"C:\draft_code\complete")
+
+os.mkdir(r"C:\includes")
+
+os.mkdir(r"C:\layouts")
+
+os.mkdir(r"C:\layouts\default")
+
+os.mkdir(r"C:\layouts\post")
+
+os.mkdir(r"C:/layouts/post/posted")
+
+os.mkdir(r"C:/site")
+
+os.rmdir(r"C:\draft_code\pending")
+
+os.rmdir(r"C:\draft_code\complete")
+
+os.rmdir(r"C:\draft_code")
+
+os.rmdir(r"C:\includes")
+
+os.rmdir(r"C:/layouts/post/posted")
+
+os.rmdir(r"C:\layouts\default")
+
+os.rmdir(r"C:\layouts\post")
+
+os.rmdir(r"C:\layouts")
+
+os.rmdir(r"C:/site")

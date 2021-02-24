8c6f1d68de30bba50f2cdbea4523e6acd0935123
Loren-coder <78279168+Loren-coder@users.noreply.github.com>
2021-02-24 17:03:29 -0500

    Create 2.py

---
+++ Assignments/Coding_Challenges/Challenge_3/2.py
@@ -0,0 +1,11 @@
+import sys
+
+argument1 = sys.argv[1]
+argument2 = sys.argv[2]
+argument3 = sys.argv[3]
+
+
+print("1+2=" + str(argument1))
+print("9-2=" + str(argument2))
+print("8x4=" + str(argument3))
+print("Which one is wrong?")

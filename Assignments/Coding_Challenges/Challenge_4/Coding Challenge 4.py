25a3527322b03454c76fda79c9458282e96b5212
Loren-coder <78279168+Loren-coder@users.noreply.github.com>
2021-03-03 17:53:54 -0500

    Update Coding Challenge 4.py

--- Assignments/Coding_Challenges/Challenge_4/Coding Challenge 4.py
+++ Assignments/Coding_Challenges/Challenge_4/Coding Challenge 4.py
@@ -1,1 +1,16 @@
+# Coding Challenge 4
+# For this coding challenge, I want you to find a particular tool that you like in arcpy. It could be a tool that you have used in GIS before or something new.
+# Try browsing the full tool list to get some insight here (click Tool Reference on the menu list to the left).
+#
+# Set up the tool to run in Python, add some useful comments, and importantly, provide some example data in your repository (try to make it open source, such as Open Streetmap, or RI GIS.
+# You can add it as a zip file to your repository.
+#
+# My only requirements are:
+#
+# Comment your code well.
+# Ensure that the code will run on my machine with only a single change to a single variable (i.e. a base folder location).
 
+import arcpy
+arcpy.env.workspace = r"F:\BI GIS\Boundary"
+
+arcpy.Union_analysis(["OUTLINE25K_POLY.shp", "muni97d.shp"], "MA_RI")

c06f0ec7eb28386a2e07ea3313729d880ae03f71
Loren-coder <78279168+Loren-coder@users.noreply.github.com>
2021-03-11 14:40:42 -0500

    Update 1.py

--- Assignments/Coding_Challenges/Challenge_3/1.py
+++ Assignments/Coding_Challenges/Challenge_3/1.py
@@ -39,3 +39,31 @@ os.rmdir(r"C:\layouts")
 os.rmdir(r"C:/site")
 
 #Feedback - technically this is correct, again a lot of manual work done to create these folder trees.
+
+import os
+
+
+root_path = r"C:\Empty"
+os.mkdir(root_path)
+
+
+first_level = ["draft_code", "includes", "layouts", "site"]
+
+for folder in first_level:
+
+    os.mkdir(os.path.join(root_path, folder))
+
+second_level = ["pending", "complete"]
+
+for folder in second_level:
+
+    os.mkdir(os.path.join(root_path, "draft_code", folder))
+
+
+os.mkdir(os.path.join(root_path, "layouts", "default"))
+os.mkdir(os.path.join(root_path, "layouts", "post"))
+os.mkdir(os.path.join(root_path, "layouts", "post", "posted"))
+
+
+import shutil
+shutil.rmtree(root_path)

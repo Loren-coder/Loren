9a2e95cc5662adbf6b2750267bdb397f09e437b0
Loren-coder <78279168+Loren-coder@users.noreply.github.com>
2021-03-11 14:30:24 -0500

    Update 3.py

--- Assignments/Coding_Challenges/Challenge_2/3.py
+++ Assignments/Coding_Challenges/Challenge_2/3.py
@@ -12,3 +12,16 @@ print("mr"+" "+str(string.count("mr")))
 # Technically correct, but this is a lot of manual work to do. I would suggest you do this in code instead, you can
 # 1. split string to list
 # 2. Count occurrences of item in teh list.
+
+
+
+string = 'hi dee hi how are you mr dee'
+
+list=string.split(" ")
+
+print(list)
+
+for i in list:
+    num=string.count(i)
+
+    print(i+" " +str(num))

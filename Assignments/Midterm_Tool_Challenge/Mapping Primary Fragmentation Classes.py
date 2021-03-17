e21796b9c7f4a8d80bffd0cf0108eb5de2437baa
Loren-coder <78279168+Loren-coder@users.noreply.github.com>
2021-03-17 16:28:32 -0400

    Update Mapping Primary Fragmentation Classes.py

--- Assignments/Midterm_Tool_Challenge/Mapping Primary Fragmentation Classes.py
+++ Assignments/Midterm_Tool_Challenge/Mapping Primary Fragmentation Classes.py
@@ -1,3 +1,15 @@
+# In this assignment, you are instructed to produce a small script tool that takes advantage of arcpy and Python. You will need to provide example data, and the code should run on all PC's. The tool needs to manipulate a dataset across three different processes, for example, extracting, modifying and exporting data. The exact workflow is entirely up to yourself. You are expected to take 3-4 hours on this coding assignment, and you should deposit your code and example files within a Github repository for feedback and grading.
+
+# The criteria are:
+
+# Cleanliness of code (10 points)
+# Functionality (10 points)
+# Appropriate use of documentation (10 points)
+# Depth of processing operation (10 points)
+# In addition, you must provide example data and minimize the amount of editing a user must make in order for the program to run (10 points).
+
+# Calculate distance to the waterbody of each house
+
 import arcpy
 
 
